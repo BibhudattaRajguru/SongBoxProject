1482ddab58e4f631d3c217992cddd1419ab7cfea
bibhudattarajguru69@gmail.com <piyushrajguru69@gmail.com>
2023-04-14 11:07:47 +0530

    Adding the Project

---
+++ OwnProject/automationTest/OwnPlayedSongs.java
@@ -0,0 +1,73 @@
+package automationTest;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class OwnPlayedSongs {
+	
+	
+	    private static WebDriver driver;
+	    private static HashMap<String, ArrayList<String>> Songbox = new HashMap<String, ArrayList<String>>();
+	    private static int MaxSongs = 3;
+
+	    public static void main(String[] args) {
+	    	WebDriverManager.chromedriver().setup();
+			WebDriver driver = new ChromeDriver();
+			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+			driver.get("http://localhost:8080"); 
+			driver.manage().window().maximize();
+
+	        SongTOplay("user1", "S1");
+	        SongTOplay ("user1", "S2");
+	        SongTOplay ("user1", "S3");
+
+	        ArrayList<String> playlist = Songbox.get("user1");
+	        if (playlist.size() == 3 && playlist.get(0).equals("S1") && playlist.get(1).equals("S2") && playlist.get(2).equals("S3")) {
+	        	 SongTOplay("user1", "S4");
+	 	        SongTOplay ("user1", "S2");
+	 	        SongTOplay ("user1", "S3");
+	            playlist = Songbox.get("user1");
+	            if (playlist.size() == 3 && playlist.get(0).equals("S4") && playlist.get(1).equals("S2") && playlist.get(2).equals("S1")) {
+	                System.out.println("Playlist is correct");
+	            } else {
+	                System.out.println("Error-  playlist is not correct");
+	            }
+	        } else {
+	            System.out.println("Error- playlist is not correct ");
+	        }
+
+
+	        driver.close();
+	        driver.quit();
+	    }
+
+	    public static void SongTOplay(String user, String song) {
+	        WebElement userE = driver.findElement(By.id("user"));
+	        userE.sendKeys(user);
+
+	        WebElement songE = driver.findElement(By.id("song"));
+	        songE.sendKeys(song);
+
+	        WebElement playButton = driver.findElement(By.id("play"));
+	        playButton.click();
+
+	        if (!Songbox.containsKey(user)) {
+	            Songbox.put(user, new ArrayList<String>());
+	        }
+	        ArrayList<String> playlist = Songbox.get(user);
+	        if (playlist.size() == MaxSongs) {
+	            playlist.remove(0);
+	        }
+	        playlist.add(song);
+	    }
+	}
+
+
